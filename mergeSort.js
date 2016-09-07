6d4a711be933c23d8b04f3ced8aabcb0143fb407
Ashok Vijayendra <ashok.vijay.blr@gmail.com>
2016-09-08 04:06:51 +0530

    Merge Sort

---
+++ mergeSort.js
@@ -0,0 +1,23 @@
+const merge = function(leftArray,rightArray){
+	const newArray = [];
+	while(leftArray.length && rightArray.length){
+		if(leftArray[0] < rightArray[0]){
+			newArray.push(leftArray.shift())
+		}else {
+			newArray.push(rightArray.shift())
+		}
+	}
+	return newArray.concat(leftArray).concat(rightArray);
+}
+
+const mergeSort = function(inputArray){
+	if(inputArray.length == 1){
+		return inputArray
+	}else {
+		const left = Math.floor(inputArray.length/2);
+		const leftArray = inputArray.splice(0,left);
+		return merge(mergeSort(leftArray),mergeSort(inputArray))
+	}
+}
+
+console.log(mergeSort([3, 5 ,9,4,9999,8]));
